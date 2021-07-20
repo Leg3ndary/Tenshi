75804aa73dd50e46b4dea899de382cf9069ec8a0
ben_z_13 <57199957+teddybears123456789@users.noreply.github.com>
2021-07-19 22:53:34 -0400

    Removed on_command listener

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ from cogs.colors import *
 load_dotenv()
 
 async def get_prefix(bot, message):
-    """Gets the prefix from built cache, if a guild isn"t found (Direct Messages) assumes prefix is the below"""
+    """Gets the prefix from built cache, if a guild isn't found (Direct Messages) assumes prefix is the below"""
     if message.guild is None:
         return "t>"
     return bot.prefix_cache[str(message.guild.id)]
@@ -28,24 +28,25 @@ bot = commands.Bot(
 
 
 cog_list = [
-    "cogs.devonly",
-    "cogs.help",
-    "cogs.anime",
-    "cogs.other",
-    "cogs.music",
-    "cogs.unsplash",
-    "cogs.systeminfo", # Done
-    "cogs.mongodb",
-    "cogs.settings",
-    "cogs.colors",
-    "cogs.moderation",
-    "cogs.language",
-    "cogs.redis",
-    "cogs.errorhandler",
-    "cogs.exalia.exalia"
+    "cogs.devonly", # basically done
+    "cogs.help", # also basically done for now
+    "cogs.anime", # need to add all the other methods sighs
+    "cogs.other", # basically done
+    "cogs.music", # not done
+    "cogs.unsplash", # not done
+    "cogs.systeminfo", # Done.
+    "cogs.mongodb", # will update as I need too
+    "cogs.settings", # will update as I need too
+    "cogs.colors", # add menu pages
+    "cogs.moderation", # Done.
+    "cogs.language", # Done. Maybe update dictionary and add a menu.
+    "cogs.redis", # Not Done.
+    "cogs.errorhandler", # Done.
+    "cogs.exalia.exalia" # Not Done.
 ]
 
 bot.cog_list = cog_list
+
 
 start = time.monotonic()
 for cog in cog_list:
@@ -54,15 +55,19 @@ end = time.monotonic()
 
 print(f"{len(cog_list)} cogs loaded in {(round((end - start) * 1000, 2))/1000} seconds.")
 
-
 @bot.event
 async def on_ready():
-    """On ready show the bot logging in."""
-    print(f"Bot Login Success.\n{bot.user}")
-
-@bot.event
-async def on_command(ctx):
-    """For detecting new users that are using our bot and quickly adding them to our database."""
+    """On ready do stuff"""
+    bot.update_channel = await bot.fetch_channel(866868897398259732)
+    embed = discord.Embed(
+       title="Cogs",
+       description=f"""```diff
++ {len(cog_list)} cogs loaded in {(round((end - start) * 1000, 2))/1000} seconds.
+```""",
+       timestamp=datetime.datetime.utcnow(),
+       color=c_get_color("green")
+    )
+    await bot.update_channel.send(embed=embed)
 
 
 bot.run(os.getenv("Token"))
