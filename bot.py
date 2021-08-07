cec67e3e62eb82a6b102461ea96a462895a76d18
ben_z_13 <57199957+teddybears123456789@users.noreply.github.com>
2021-08-07 16:19:06 -0400

    Added file length to bot

--- bot.py
+++ bot.py
@@ -19,6 +19,40 @@ async def get_prefix(bot, message):
         return "t>"
     return bot.prefix_cache[str(message.guild.id)]
 
+def len_file(file):
+    """Return the file length for a given file"""
+    try:
+        with open(file, encoding="utf8") as f:
+            for i, l in enumerate(f):
+                pass
+        return i + 1
+    except Exception as e:
+        print(e)
+        return 0
+
+def get_files(directory: str=None):
+    """Return every file using recursion"""
+    files = []
+    if directory:
+        if directory == "__pycache__":
+            pass
+        else:
+            directories = os.listdir(directory)
+            filepath = directory + "/"
+    else:
+        filepath = ""
+        directories = os.listdir()
+    for file in directories:
+        if file.endswith(".exe") or file.endswith(".png") or file.endswith(".pyc"):
+            pass
+        elif "." not in file:
+            recursion = get_files(f"{filepath}{file}")
+            files = files + recursion
+        else:
+            files.append(f"{filepath}{file}")
+    print(files)
+    return files
+
 
 bot = commands.Bot(
     command_prefix=get_prefix,
@@ -47,6 +81,20 @@ cog_list = [
 
 bot.cog_list = cog_list
 
+file_len_list = {}
+total = 0
+
+start = time.monotonic()
+for file in get_files():
+    file_len = len_file(file)
+    file_len_list[file] = file_len
+    total += file_len
+file_len_list["total"] = total 
+end = time.monotonic()
+
+print(f"{len(file_len_list)} files length loaded in {(round((end - start) * 1000, 2))/1000} seconds.")
+
+bot.file_len_dict = file_len_list
 
 start = time.monotonic()
 for cog in cog_list:
