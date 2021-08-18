212e3f9fa46f403b1659e45cfe53b3d703fd6fba
ben_z_13 <57199957+teddybears123456789@users.noreply.github.com>
2021-08-17 22:55:05 -0400

    Cog template that I can quickly copy

---
+++ cogs/cog_template.py
@@ -0,0 +1,19 @@
+"""
+Tenshi Discord Bot
+Copyright (C) 2021 Ben Z.
+This software is licensed under Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International (CC BY-NC-ND 4.0)
+"""
+
+import discord
+from discord.ext import commands
+import datetime
+from gears.cosmetics import *
+
+
+class CogName(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+
+def setup(bot):
+    bot.add_cog(CogName(bot))
